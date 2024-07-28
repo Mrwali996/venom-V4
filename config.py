7fd215501339c85671b37bff0565d8024059b973
Mrwali996 <144596146+Mrwali996@users.noreply.github.com>
2024-07-28 21:34:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Don't use quotes( " and ' )
-#SCRIPT BY VENOMxCRAZY
+#SCRIPT BY Neyooo
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7362675940:AAEtWNxVCvB4-r8e2SIOzStKLQCAbvyDCaU")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Neyooo00")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6370066138")
 
 
 
